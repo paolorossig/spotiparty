6e227309d4f22e1429ccd1b5d8faaa695afdeb7d
PaoloRossi <paolo.rossi@pucp.edu.pe>
2022-05-15 19:50:05 -0500

    feat: add meta for open graph and twitter

--- src/components/Layouts/LandingLayout/index.tsx
+++ src/components/Layouts/LandingLayout/index.tsx
@@ -12,6 +12,20 @@ const LandingLayout = ({ children }: Props) => {
       <Head>
         <title>Spotiparty</title>
         <link rel="icon" href="/icon.png" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@paolo_rossi_g" />
+        <meta name="twitter:creator" content="@paolo_rossi_g" />
+        <meta property="og:url" content="https://spotiparty.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Spotiparty | Best app for Karaoke" />
+        <meta
+          property="og:description"
+          content="App that randomly creates playlists for Karaoke based on users recently played songs."
+        />
+        <meta
+          property="og:image"
+          content="https://spotiparty.vercel.app/thumbnail.png"
+        />
       </Head>
 
       <Header />
