15c41f1f7704f4e03083b0cebc9e3cf75a2ae495
PaoloRossi <paolo.rossi@pucp.edu.pe>
2022-05-17 20:19:08 -0500

    refactor: extract ShareRoom into a component

---
+++ src/features/rooms/ShareRoom.tsx
@@ -0,0 +1,69 @@
+import clsx from 'clsx'
+import toast from 'react-hot-toast'
+import { QRCodeSVG } from 'qrcode.react'
+import { BsChevronDown, BsClipboard, BsClipboardCheck } from 'react-icons/bs'
+import useToggle from '@hooks/useToggle'
+import useCopyToClipboard from '@hooks/useCopyToClipboard'
+import type { Room } from '@definitions/rooms'
+
+const ShareRoom = ({ room }: { room: Room }) => {
+  const [showQrCode, toggleQrCode] = useToggle(true)
+  const [copiedText, copy] = useCopyToClipboard()
+
+  const copyLink = () => {
+    copy(room.linkUrl ?? '')
+    toast.success('Link copied succesfully.')
+  }
+
+  return (
+    <section className="relative flex flex-col-reverse items-center rounded-lg border-2 border-gray-700 lg:flex-row">
+      <button
+        onClick={toggleQrCode}
+        className={clsx(
+          'absolute right-2 top-2 grid h-8 w-8 transform place-content-center rounded-full bg-gray-700 transition duration-300 hover:bg-gray-800',
+          showQrCode ? 'rotate-180' : 'rotate-0',
+          'lg:hidden'
+        )}
+      >
+        <BsChevronDown className="text-xl text-white" />
+      </button>
+      <div
+        className={clsx(
+          'm-2 flex-1 items-center justify-center gap-2 text-sm md:my-4 md:text-base',
+          showQrCode ? 'flex' : 'hidden'
+        )}
+      >
+        <div className="flex flex-col items-center">
+          <p className="text-center">
+            Join at spotiparty.vercel.app/app/room/join
+          </p>
+          <p>or scan the QR Code</p>
+        </div>
+        <QRCodeSVG value={room.linkUrl} size={120} />
+      </div>
+      <div className="m-2 flex flex-1 flex-col items-center md:my-4">
+        <h3 className="">Room ID:</h3>
+        <div className="flex items-center gap-2">
+          <h1 className="text-3xl font-bold">{room._id}</h1>
+          <div className="rounded-full p-3 hover:cursor-pointer hover:bg-gray-600">
+            {!copiedText ? (
+              <BsClipboard
+                onClick={copyLink}
+                className="text-xl"
+                strokeWidth={'0.5'}
+              />
+            ) : (
+              <BsClipboardCheck
+                onClick={copyLink}
+                className="text-xl"
+                strokeWidth={'0.5'}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ShareRoom
--- src/pages/app/rooms/[roomId].tsx
+++ src/pages/app/rooms/[roomId].tsx
@@ -1,16 +1,11 @@
-import clsx from 'clsx'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import { toast } from 'react-hot-toast'
-import { QRCodeSVG } from 'qrcode.react'
 import { IoMdMicrophone } from 'react-icons/io'
-import { BsClipboard, BsClipboardCheck, BsChevronDown } from 'react-icons/bs'
-import useToggle from '@hooks/useToggle'
-import useCopyToClipboard from '@hooks/useCopyToClipboard'
 import { useGetRoombyIdQuery } from '@features/rooms/roomApi'
 import { getSingleRoomResponse } from '@definitions/rooms'
 import Toaster from '@components/Toaster'
 import AppLayout from '@components/Layouts/AppLayout'
+import ShareRoom from '@features/rooms/ShareRoom'
 
 const Room = () => {
   const router = useRouter()
@@ -20,14 +15,6 @@ const Room = () => {
     (roomId as string) || ''
     // { pollingInterval: 3000 }
   )
-
-  const [copiedText, copy] = useCopyToClipboard()
-  const copyLink = () => {
-    copy(response?.data?.linkUrl ?? '')
-    toast.success('Link copied succesfully.')
-  }
-
-  const [showQrCode, toggleQrCode] = useToggle(true)
 
   return (
     <AppLayout>
@@ -43,85 +30,45 @@ const Room = () => {
         </div>
       ) : (
         <>
-          <section className="relative flex flex-col-reverse items-center rounded-lg border-2 border-gray-700 lg:flex-row">
-            <button
-              onClick={toggleQrCode}
-              className={clsx(
-                'absolute right-2 top-2 grid h-8 w-8 transform place-content-center rounded-full bg-gray-700 transition duration-300 hover:bg-gray-800',
-                showQrCode ? 'rotate-180' : 'rotate-0',
-                'lg:hidden'
-              )}
-            >
-              <BsChevronDown className="text-xl text-white" />
-            </button>
-            <div
-              className={clsx(
-                'm-2 flex-1 items-center justify-center gap-2 text-sm md:my-4 md:text-base',
-                showQrCode ? 'flex' : 'hidden'
-              )}
-            >
-              <div className="flex flex-col items-center">
-                <p className="text-center">
-                  Join at spotiparty.vercel.app/app/room/join
-                </p>
-                <p>or scan the QR Code</p>
-              </div>
-              <QRCodeSVG value={response.data.linkUrl} size={120} />
+          <ShareRoom room={response.data} />
+          <br />
+          <section className="flex flex-col gap-6">
+            <div>
+              <h1 className="mb-2 text-4xl font-bold">
+                {response.data.name} Room
+              </h1>
+              <p className="text-xl text-gray-300">
+                {response.data.description}
+              </p>
             </div>
-            <div className="m-2 grid flex-1 place-content-center md:my-4">
-              <div className="flex flex-col items-center">
-                <h3 className="">Room ID:</h3>
-                <div className="flex items-center gap-2">
-                  <h1 className="text-3xl font-bold">{roomId}</h1>
-                  <div className="rounded-full p-3 hover:cursor-pointer hover:bg-gray-600">
-                    {!copiedText ? (
-                      <BsClipboard
-                        onClick={copyLink}
-                        className="text-xl"
-                        strokeWidth={'0.5'}
-                      />
-                    ) : (
-                      <BsClipboardCheck
-                        onClick={copyLink}
-                        className="text-xl"
-                        strokeWidth={'0.5'}
-                      />
+            <div>
+              <p className="text-gray-300">Members:</p>
+              <hr className="my-1 border-gray-700" />
+              <ul className="m-4 flex flex-col gap-4">
+                {response.data.members?.map((member) => (
+                  <li
+                    key={member.accountId}
+                    className="flex items-center gap-4"
+                  >
+                    <Image
+                      src={
+                        member.image ||
+                        'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png'
+                      }
+                      alt="Profile image"
+                      width={40}
+                      height={40}
+                      className="rounded-full object-cover"
+                    />
+                    <h3>{member.name}</h3>
+                    {member.role === 'owner' && (
+                      <IoMdMicrophone className="text-2xl text-yellow-400" />
                     )}
-                  </div>
-                </div>
-              </div>
+                  </li>
+                ))}
+              </ul>
             </div>
           </section>
-          <br />
-          <div>
-            <h1 className="mb-2 text-4xl font-bold">
-              {response.data.name} Room
-            </h1>
-            <p className="text-xl text-gray-300">{response.data.description}</p>
-            <br />
-            <p className="text-gray-300">Members:</p>
-            <hr className="my-1 border-gray-700" />
-            <ul className="m-4 flex flex-col gap-4">
-              {response.data.members.map((member) => (
-                <li key={member.accountId} className="flex items-center gap-4">
-                  <Image
-                    src={
-                      member.image ||
-                      'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png'
-                    }
-                    alt="Profile image"
-                    width={40}
-                    height={40}
-                    className="rounded-full object-cover"
-                  />
-                  <h3>{member.name}</h3>
-                  {member.role === 'owner' && (
-                    <IoMdMicrophone className="text-2xl text-yellow-400" />
-                  )}
-                </li>
-              ))}
-            </ul>
-          </div>
           <Toaster />
         </>
       )}
