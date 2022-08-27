f4d3653749d2a8e8fcc4edcab463955282763906
PaoloRossi <paolo.rossi@pucp.edu.pe>
2022-08-27 15:01:06 -0500

    fix: update and remove dependencies

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,12 +1,5 @@
-import type mongoose from 'mongoose'
 import type { PrismaClient } from '@prisma/client'
 
-interface mongooseProps {
-  conn?: mongoose
-  promise: Promise
-}
-
 declare global {
-  var mongoose: mongooseProps
   var prisma: PrismaClient | undefined
 }
