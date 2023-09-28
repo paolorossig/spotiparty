0864fb34e3ea9d7b2ecb1638022657204ae2b2d5
Paolo Rossi <paolo.rossi@pucp.edu.pe>
2023-09-28 11:38:03 -0500

    feat: add env-nextjs for typesafety in envs

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 
+import { env } from '@/env.mjs'
+
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
@@ -8,11 +10,9 @@ export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
     log:
-      process.env.NODE_ENV === 'development'
-        ? ['query', 'error', 'warn']
-        : ['error'],
+      env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   })
 
-if (process.env.NODE_ENV !== 'production') {
+if (env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = db
 }
--- src/server/services/auth.ts
+++ src/server/services/auth.ts
@@ -4,6 +4,7 @@ import { getServerSession, type Account, type NextAuthOptions } from 'next-auth'
 import { type JWT } from 'next-auth/jwt'
 import SpotifyProvider from 'next-auth/providers/spotify'
 
+import { env } from '@/env.mjs'
 import { LOGIN_URL } from '@/lib/spotify'
 import { db } from '@/server/db'
 
@@ -18,8 +19,8 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
   providers: [
     SpotifyProvider({
-      clientId: process.env.SPOTIFY_CLIENT_ID,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      clientId: env.SPOTIFY_CLIENT_ID,
+      clientSecret: env.SPOTIFY_CLIENT_SECRET,
       authorization: LOGIN_URL,
     }),
   ],
